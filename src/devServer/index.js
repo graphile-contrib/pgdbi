ab50274b209f222972505f583a9f96ffdd55b312
Benjie Gillam <benjie@jemjie.com>
2019-04-05 09:23:21 +0100

    Move pgdbi from separate server to subpath

--- src/devServer/index.js
+++ src/devServer/index.js
@@ -1,20 +1,42 @@
-require('./.env')
+try {
+  require('./.env')
+} catch (e) {
+  // No envvars 🤷
+}
 const express = require("express");
-const {postgraphile} = require("postgraphile");
+const {postgraphile, makePluginHook} = require("postgraphile");
 
+const PGI = require('../pgi')
 const plugins = [
   // require('postgraphile-plugin-connection-filter'),
-  require('../pgi')
 ]
 
-const port = process.env.PORT
-const connection = process.env.POSTGRES_CONNECTION
-const schemas = process.env.POSTGRAPHILE_SCHEMAS.split(',')
-const dynamicJson = process.env.DYNAMIC_JSON === 'true'
-const disableDefaultMutations = process.env.DISABLE_DEFAULT_MUTATIONS === 'true'
-const watchPg = process.env.WATCH_PG === 'true'
-const enablePgdbi = process.env.ENABLE_PGDBI === 'true'
+const isProd = process.env.NODE_ENV === 'production';
 
+const {
+  PORT = 3000,
+  POSTGRES_CONNECTION,
+  POSTGRAPHILE_SCHEMAS = "public",
+  DYNAMIC_JSON = 'true',
+  DISABLE_DEFAULT_MUTATIONS = null,
+  WATCH_PG = isProd ? null : 'true',
+  ENABLE_PGDBI = isProd ? null : 'true'
+} = process.env;
+
+if (!POSTGRES_CONNECTION) {
+  throw new Error("No 'POSTGRES_CONNECTION' envvar found, we don't know which database to connect to.");
+}
+
+
+const port = PORT;
+const connection = POSTGRES_CONNECTION;
+const schemas = POSTGRAPHILE_SCHEMAS.split(',');
+const dynamicJson = DYNAMIC_JSON === 'true';
+const disableDefaultMutations = DISABLE_DEFAULT_MUTATIONS === 'true';
+const watchPg = WATCH_PG === 'true';
+const enablePgdbi = ENABLE_PGDBI === 'true';
+
+const pluginHook = makePluginHook([PGI]);
 
 const app = express();
 
@@ -22,23 +44,31 @@ app.use(postgraphile(
   connection
   ,schemas
   ,{
-    dynamicJson: dynamicJson
+    pluginHook
+    ,dynamicJson: dynamicJson
     ,showErrorStack: true
     ,extendedErrors: ['severity', 'code', 'detail', 'hint', 'positon', 'internalPosition', 'internalQuery', 'where', 'schema', 'table', 'column', 'dataType', 'constraint', 'file', 'line', 'routine']
     ,disableDefaultMutations: disableDefaultMutations
     ,appendPlugins: plugins
     ,watchPg: watchPg
     ,graphiql: true
-    ,graphileBuildOptions: {
-      pgdbiOptions: {
-        enable: enablePgdbi,
-        connection: connection,
-        // port: 5679
-      }
-    }
+    ,enablePgdbi
   }
 ));
 
-app.listen(port)
+const server = app.listen(port)
+
+/*
+ * When being used in nodemon, SIGUSR2 is issued to restart the process. We
+ * listen for this and shut down cleanly.
+ */
+process.once('SIGUSR2', function () {
+  server.close();
+  const t = setTimeout(function () {
+    process.kill(process.pid, 'SIGUSR2');
+  }, 200);
+  // Don't prevent clean shutdown:
+  t.unref();
+});
 
 console.log(`listening on ${port}`)
