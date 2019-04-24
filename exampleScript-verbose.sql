c3033d16df3d363f97af6f0eff55762352fa4445
stlbucket <stlbucket@gmail.com>
2019-04-23 18:04:55 -0700

    ex scripts

---
+++ exampleScript-verbose.sql
@@ -0,0 +1,96 @@
+
+  ---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+  ---------------------------------------------------------------------------------------------------------------------------------------------------------------------  BEGIN: org.company
+  ---------------------------------------------------------------------------------------------------------------------------------------------------------------------  POLICY NAME:  Custom Policy: org.company
+  ---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+  ---------------------------------------------------------------------------------------------------------------------------------------------------------------------  REMOVE EXISTING TABLE GRANTS
+  
+    revoke all privileges on table org.company from public;
+    revoke all privileges on table org.company from app_super_admin;
+    revoke all privileges on table org.company from app_admin;
+    revoke all privileges on table org.company from app_user;
+    revoke all privileges on table org.company from app_anonymous;
+    revoke all privileges on table org.company from app_demon;
+
+  ---------------------------------------------------------------------------------------------------------------------------------------------------------------------  ENABLE ROW LEVEL SECURITY
+  
+    alter table org.company enable row level security;
+  
+    create policy rls_special_app_admin_policy_that_will_make_things_confusing_org_company 
+      on org.company
+      as permissive
+      for insert
+      to app_admin
+      using ( owner_id = viewer_id() )
+      ;
+    create policy rls_is_the_best_s_org_company 
+      on org.company
+      as permissive
+      for all
+      to app_user
+      using ( owner_id = this-can-change-in-settings.viewer_id() )
+      with check something_special
+      ;
+    create policy rls_something_else_org_company 
+      on org.company
+      as restrictive
+      for all
+      to app_user
+      using (another_function())
+      ;
+  
+  
+  ---------------------------------------------------------------------------------------------------------------------------------------------------------------------  CREATE NEW TABLE GRANTS
+
+  ---------------------------------------------------------------------------------------------------------------------------------------------------------------------  app_super_admin
+    grant 
+      delete  
+    on table org.company 
+    to app_super_admin;
+
+
+  ---------------------------------------------------------------------------------------------------------------------------------------------------------------------  app_user
+    grant 
+      select , 
+      insert ( id, external_id, licenses, company_type, name, updated_at, location_id ), -- excluded columns: created_at
+      update ( external_id, licenses, company_type, name, updated_at, location_id ), -- excluded columns: id, created_at
+    on table org.company 
+    to app_user;
+
+  
+  ---------------------------------------------------------------------------------------------------------------------------------------------------------------------  IMPLIED TABLE GRANTS
+
+    -------------------------------------------------------------------------------------------------------------------------------------------------------------------  app_super_admin
+    --IMPLIED:   grant select on table org.company to app_super_admin;
+    --IMPLIED:   grant insert on table org.company to app_super_admin;
+    --IMPLIED:   grant update on table org.company to app_super_admin;
+
+    -------------------------------------------------------------------------------------------------------------------------------------------------------------------  app_admin
+    --IMPLIED:   grant select on table org.company to app_admin;
+    --IMPLIED:   grant insert on table org.company to app_admin;
+    --IMPLIED:   grant update on table org.company to app_admin;
+  
+    -------------------------------------------------------------------------------------------------------------------------------------------------------------------  DENIED TABLE GRANTS
+
+    -------------------------------------------------------------------------------------------------------------------------------------------------------------------  app_admin
+    --DENIED:   grant delete on table org.company to app_admin;
+
+    -------------------------------------------------------------------------------------------------------------------------------------------------------------------  app_user
+    --DENIED:   grant delete on table org.company to app_user;
+
+    -------------------------------------------------------------------------------------------------------------------------------------------------------------------  app_anonymous
+    --DENIED:   grant select on table org.company to app_anonymous;
+    --DENIED:   grant insert on table org.company to app_anonymous;
+    --DENIED:   grant update on table org.company to app_anonymous;
+    --DENIED:   grant delete on table org.company to app_anonymous;
+
+    -------------------------------------------------------------------------------------------------------------------------------------------------------------------  app_demon
+    --DENIED:   grant select on table org.company to app_demon;
+    --DENIED:   grant insert on table org.company to app_demon;
+    --DENIED:   grant update on table org.company to app_demon;
+    --DENIED:   grant delete on table org.company to app_demon;
+  
+  ---------------------------------------------------------------------------------------------------------------------------------------------------------------------  END: org.company
+  --==
+  
