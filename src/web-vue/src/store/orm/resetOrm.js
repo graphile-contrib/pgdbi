d23576e09c9886bfd43788b91f0d2b5bfe49eb47
stlbucket <stlbucket@gmail.com>
2019-05-07 14:31:30 -0700

    reset orm

--- src/web-vue/src/store/orm/resetOrm.js
+++ src/web-vue/src/store/orm/resetOrm.js
@@ -2,9 +2,6 @@ import Schema from './models/schema'
 import SchemaTable from './models/schemaTable'
 
 function resetOrm(){
-  console.log('wtf')
-  // SchemaTable.softDelete(r => { return r !== null })
-  // Schema.softDelete(r => { return r !== null })
   SchemaTable.deleteAll()
   Schema.deleteAll()
 }
