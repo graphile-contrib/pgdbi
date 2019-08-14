af7bec30c05ba8ea72374580e8daf87132db4268
stlbucket <stlbucket@gmail.com>
2019-08-14 16:04:19 -0700

    better search

--- src/web-vue/src/components/Function/FunctionList.vue
+++ src/web-vue/src/components/Function/FunctionList.vue
@@ -5,17 +5,31 @@
     hide-actions
   >
     <template slot="items" slot-scope="props">
+      <td @click="props.expanded = !props.expanded">
+        <v-btn icon @click.stop="props.expanded = !props.expanded">
+          <v-icon>{{ props.expanded ? 'expand_less' : 'expand_more' }}</v-icon>
+        </v-btn>
+        {{ props.item.name }}
+      </td>
       <td>{{props.item.functionSchema}}</td>
       <td>{{props.item.functionName}}</td>
       <td><router-link :to="{ name: 'function', params: { id: props.item.id }}" target="_blank"><v-icon>launch</v-icon></router-link></td>
+    </template>
+
+    <template slot="expand" slot-scope="props">
+      <function :id="props.item.id">
+      </function>
     </template>
   </v-data-table>
 </template>
 
 <script>
+  import Function from './Function'
+
   export default {
     name: 'FunctionList',
     components: {
+      Function
     },
     props: {
       functions: {
@@ -27,6 +41,10 @@
     },
     data: () => ({
       headers: [
+        {
+          text: ''
+          ,sortable: false
+        },
         {
           text: 'schema'
           ,sortable: false
--- src/web-vue/src/views/SearchView.vue
+++ src/web-vue/src/views/SearchView.vue
@@ -3,14 +3,15 @@
     ma-0 
     pa-0
   >
+    <h2>Enter a search term to find functions of interest</h2>
     <v-text-field 
       label="Search Term"
       v-model="searchTerm"
     ></v-text-field>
-    <v-btn 
+    <!-- <v-btn 
       @click="performSearch"
       :disabled="disabled"
-    >Search</v-btn>
+    >Search</v-btn> -->
     <hr>
     <function-list
       :functions="functions"
@@ -32,27 +33,49 @@
     computed: {
       disabled () {
         return this.searchTerm === undefined || this.searchTerm === null || this.searchTerm === ''
+      },
+      allFunctions () {
+        return this.$store.state.managedSchemata.reduce(
+          (all, schema) => {
+            return [
+              ...all,
+              ...schema.schemaFunctions
+            ]
+          }, []
+        )
+      },
+      functions () {
+        console.log(this.allFunctions)
+        return this.allFunctions.filter(
+          f => {
+            return f.functionSchema.toLowerCase().indexOf(this.searchTerm.toLowerCase()) !== -1 ||
+            f.functionName.toLowerCase().indexOf(this.searchTerm.toLowerCase()) !== -1 ||
+            f.definition.toLowerCase().indexOf(this.searchTerm.toLowerCase()) !== -1
+          }
+        )
       }
     },
+    watch: {
+    },
     data: () => ({
-      searchTerm: 'into SCHEMA.TABLE',
-      functions: []
+      searchTerm: '',
+      // functions: []
     }),
     methods: {
-      performSearch () {
-        this.$apollo.mutate({
-          mutation: searchFunctions,
-          variables: {
-            searchTerm: this.searchTerm
-          }
-        })
-        .then(result => {
-          this.functions = result.data.searchFunctions
-        })
-        .catch(error => {
-          console.log(error)
-        })
-      }
+      // performSearch () {
+      //   this.$apollo.mutate({
+      //     mutation: searchFunctions,
+      //     variables: {
+      //       searchTerm: this.searchTerm
+      //     }
+      //   })
+      //   .then(result => {
+      //     this.functions = result.data.searchFunctions
+      //   })
+      //   .catch(error => {
+      //     console.log(error)
+      //   })
+      // }
     },
   }
 </script>
