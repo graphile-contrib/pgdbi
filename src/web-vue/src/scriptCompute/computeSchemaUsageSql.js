15a4d926fb14a9a80a93eca6433725c22404072d
Kevin Burkett <stlbucket@gmail.com>
2020-05-29 11:09:19 -0700

    alpha93

--- src/web-vue/src/scriptCompute/computeSchemaUsageSql.js
+++ src/web-vue/src/scriptCompute/computeSchemaUsageSql.js
@@ -2,8 +2,20 @@ import Mustache from "mustache"
 
 const schemaUsageSqlTemplate = `
 ---  schema usage
+REVOKE USAGE ON SCHEMA public FROM PUBLIC;
+
+GRANT USAGE ON SCHEMA public TO {{appAuthenticator}};
+
 {{#schemata}}
 ------- {{schemaName}}
+  REVOKE USAGE ON SCHEMA {{schemaName}} FROM PUBLIC;
+  {{#rolesToRevoke}}
+  REVOKE USAGE ON SCHEMA {{schemaName}} FROM {{roleName}}
+  {{/rolesToRevoke}}
+
+  {{#rolesToGrant}}
+  GRANT USAGE ON SCHEMA {{schemaName}} FROM {{roleName}}
+  {{/rolesToGrant}}
 {{/schemata}}
 `
 
@@ -71,9 +83,12 @@ const computeSchemaUsageSql = (state) => {
         return roles.length > 0 ? mergeArrays(all, roles) : all
       }, []
     )
+    const rolesToRevoke = [state.dbAuthenticatorRole, ...state.dbUserRoles]
     const rolesToGrant = mergeArrays(rolesToGrantTable, rolesToGrantFunction).join(', ')
     return {
       ...s,
+      appAuthenticator: state.dbAuthenticatorRole.roleName,
+      rolesToRevoke: rolesToRevoke,
       rolesToGrant: rolesToGrant
     }
   })
@@ -85,68 +100,5 @@ const computeSchemaUsageSql = (state) => {
     }
   )
 }
-
-
-// const computeSchemaUsageSql = (state) => {
-//   return `
-//   ${state.managedSchemata
-//   .sort((a,b)=>{
-//     if ( a.schemaName < b.schemaName ){
-//       return -1;
-//     }
-//     if ( a.schemaName > b.schemaName ){
-//       return 1;
-//     }
-//     return 0;
-
-//   })
-//   .map(s => {
-//     const rolesToGrantTable = Object.keys(state.tablePolicyAssignments)
-//     .filter(k => k.split('.')[0] === s.schemaName)
-//     .reduce(
-//       (all, tableName, i) => {
-//         const tpa = state.tablePolicyAssignments[tableName]
-//         const policy = state.policies.find(p => p.id === tpa.policyDefinitionId)
-//         const roles = Object.keys(policy.roleGrants)
-//         .filter(
-//           r => {
-//             // console.log(Object.values(policy.roleGrants[r]), Object.values(policy.roleGrants[r]).indexOf('ALLOWED') > -1)
-//             return Object.values(policy.roleGrants[r]).indexOf('ALLOWED') > -1
-//           }
-//         )
-//         // console.log(JSON.stringify(policy,0.2))
-//         return roles.length > 0 ? mergeArrays(all, roles) : all
-//       }, []
-//     )
-
-//     const rolesToGrantFunction = Object.keys(state.functionPolicyAssignments)
-//     .filter(k => k.split('.')[0] === s.schemaName)
-//     .reduce(
-//       (all, functionName, i) => {
-//         const fpa = state.functionPolicyAssignments[functionName]
-//         const policy = state.functionPolicies.find(p => p.id === fpa.policyDefinitionId)
-//         const roles = Object.keys(policy.roleFunctionGrants)
-//         .filter(
-//           r => {
-//             // console.log(Object.values(policy.roleGrants[r]), Object.values(policy.roleGrants[r]).indexOf('ALLOWED') > -1)
-//             return Object.values(policy.roleFunctionGrants[r]).indexOf('ALLOWED') > -1
-//           }
-//         )
-//         // console.log(JSON.stringify(policy,0.2))
-//         return roles.length > 0 ? mergeArrays(all, roles) : all
-//       }, []
-//     )
-//     const rolesToGrant = mergeArrays(rolesToGrantTable, rolesToGrantFunction).join(', ')
-        
-//     return `
-// -----   ${s.schemaName}
-//   REVOKE 
-//     ${rolesToGrant.length > 0 ?
-//       `GRANT USAGE ON SCHEMA ${s.schemaName} TO ${rolesToGrant};` : 
-//        '-- no usage granted for schema'}    
-// `
-//   }).join('')}
-// `
-// }
 
 export default computeSchemaUsageSql
